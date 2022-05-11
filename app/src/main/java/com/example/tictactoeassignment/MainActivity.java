9deb3ce9e8647563475676e35898b58f7723d620
NoyHadad <noy.hadad22@cs.colman.ac.il>
2022-05-11 23:55:14 +0300

    tried to not hard-code some strings (not possible because context is not available there - not in activity)
    MUST reedit cause it's ugly

--- app/src/main/java/com/example/tictactoeassignment/MainActivity.java
+++ app/src/main/java/com/example/tictactoeassignment/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tictactoeassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.Button;
